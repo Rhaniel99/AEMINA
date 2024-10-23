c20397970ee1647762a6ae47eb9d1729afd98053
rhaniel99 <rhanielmonteiro.13@gmail.com>
2024-10-23 16:30:23 -0400

    ajustando a felcidade alheia

--- src/resources/js/Layouts/Layout.jsx
+++ src/resources/js/Layouts/Layout.jsx
@@ -108,7 +108,7 @@ export default function Layout({ children }) {
             )}
 
             {!auth.user && (
-                <Modal open={loginOpen} onClose={() => setLoginOpen(false)}>
+                <Modal open={loginOpen} onClose={() => setLoginOpenLayout(false)}>
                     <section>
                         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
                             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
---
+++ src/resources/js/app/layout.jsx
@@ -0,0 +1,14 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+
+export default function Layout({ children }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
