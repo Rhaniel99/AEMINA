9a39f0b57d27a7451e9bd4d1919839171163b516
rhaniel99 <rhanielmonteiro.13@gmail.com>
2024-12-26 16:36:36 -0400

    adicionado funcao de update

--- src/resources/js/components/modal/index.jsx
+++ src/resources/js/components/modal/index.jsx
@@ -4,21 +4,21 @@ export default function Modal({ open, onClose, children }) {
             <div
                 onClick={onClose}
                 className={`fixed inset-0 flex justify-center items-center transition-colors 
-                    ${open ? "visible bg-black/50" : "invisible"}`} // Alterado para bg-black/50 para melhor contraste
+                    ${open ? "visible bg-black/50" : "invisible"}`}
             >
                 {/* Modal */}
                 <div
                     onClick={(e) => e.stopPropagation()}
-                    className={`bg-white dark:bg-[#CB665A] dark:border-[#F3BD97] rounded-xl shadow-lg p-6 transition-all 
+                    className={`bg-white dark:bg-[#CB665A] dark:border-[#F3BD97] rounded-xl shadow-lg p-2 transition-all 
                         ${
                             open
                                 ? "scale-100 opacity-100"
                                 : "scale-125 opacity-0"
-                        }`} // Fundo branco adicionado
+                        }`}
                 >
                     <button
                         onClick={onClose}
-                        className="absolute top-2 right-2 p-1 rounded-full text-gray-600 hover:bg-gray-200 hover:text-gray-800 transition-colors duration-200"
+                        className="absolute top-2 right-2 p-2 rounded-full text-gray-600 hover:bg-gray-200 hover:text-gray-800 transition-colors duration-200"
                     >
                         X
                     </button>
