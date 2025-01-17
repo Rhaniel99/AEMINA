2277102bd6cc008542d51e031efcc8a9bd320627
rhaniel99 <rhanielmonteiro.13@gmail.com>
2025-01-17 11:54:02 -0400

    commitada braba

--- src/app/Jobs/UploadMediaJob.php
+++ src/app/Jobs/UploadMediaJob.php
@@ -29,41 +29,54 @@ class UploadMediaJob implements ShouldQueue
     public function handle()
     {
         ini_set('memory_limit', '-1');
-
+    
         // Gerar o caminho correto para o arquivo recodificado
         $recodedFilePath = storage_path("app/private/tus/recoded_" . basename($this->filePath));
-
+    
+        if (!file_exists($this->filePath)) {
+            Log::warning('Arquivo de entrada não encontrado: ' . $this->filePath);
+            return;
+        }
+        
         // Construir o comando FFmpeg para recodificar o vídeo
-        $command = "ffmpeg -i {$this->filePath} -c:v libx264 -profile:v high10 -c:a aac -level 4.2 -y {$recodedFilePath} 2>&1";
-        // $command = "ffmpeg -i {$this->filePath} -c:v libx264 -c:a aac -profile:v main -level 4.2 -y {$recodedFilePath} 2>&1";
+        // ffmpeg -i arquivo_entrada.mp4 -c:v libx264 -preset medium -crf 23 -profile:v high -c:a aac -b:a 128k arquivo_saida.mp4
+        // ffmpeg -i input.mp4 -c:v libx264 -profile:v high10 -pix_fmt yuv420p10le output.mp4
 
+        $command = "ffmpeg -loglevel debug -i {$this->filePath} -c:v libx264 -profile:v high10 -pix_fmt yuv420p10le {$recodedFilePath}";
+
+        // $command = "ffmpeg -i {$this->filePath} -c:v libx264 -preset medium -crf 23 -profile:v main -c:a aac -b:a 128k -movflags +faststart {$recodedFilePath}";
+    
         Log::info('Iniciando recodificação do vídeo: ' . $this->filePath);
         $startTime = microtime(true);
-
+    
         // Executar o FFmpeg
-        exec($command, $output, $return_var);
-
+        Log::info('Comando FFmpeg: ' . $command);
+        exec($command . ' 2>&1', $output, $return_var);
+        Log::info('Saída do FFmpeg: ' . implode("\n", $output));
+    
         // Calcular o tempo de execução
         $endTime = microtime(true);
         $executionTime = $endTime - $startTime;
         Log::info('Tempo de execução do FFmpeg: ' . $executionTime . ' segundos');
-
+    
         if ($return_var !== 0) {
-            Log::warning('Erro ao recodificar o vídeo: ' . implode("\n", $output));
+            Log::warning('Erross ao recodificar o vídeo: ' . implode("\n", $output));
             return;
         }
 
 
-        // Armazenar o arquivo recodificado no MinIO (S3)
-        $fileContents = file_get_contents($recodedFilePath);
-
+    
         // Recuperar a mídia associada
         $mediaFile = MediaFiles::find($this->mediaFileId);
-
+    
         if ($mediaFile) {
-            Storage::disk('s3')->put($mediaFile->file_path, $fileContents);
-
-            // Opcional: Apagar o arquivo recodificado localmente após o upload
+            // Usar stream para enviar o arquivo recodificado para o S3
+            Storage::disk('s3')->put(
+                $mediaFile->file_path,
+                fopen($recodedFilePath, 'r')
+            );
+    
+            // Apagar o arquivo recodificado localmente após o upload
             unlink($recodedFilePath);
         } else {
             Log::warning('Mídia não encontrada para ID: ' . $this->mediaFileId);
